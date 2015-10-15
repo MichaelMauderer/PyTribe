3de123848d876d96d9cd42a81d802d69c865e697
Michael Mauderer <mail@michaelmauderer.com>
2015-10-15 10:26:22 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pytribe',
+      version='3',
+      description='Python wrapper for the EyeTribe SDK.',
+      author='Edwin Dalmaijer',
+      py_modules=['pytribe'],
+     )
